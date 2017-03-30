5722918a7ddd6110476338d3aa168fe050aa6038
JustinWeq <zoomdude111@hotmail.com>
2017-03-30 14:17:02 -0700

    Adding some test cases for editing the user

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -9,8 +9,22 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   test "unsuccessfull edit" do
     get edit_user_path(@user)
     assert_template 'users/edit'
-    patch user_path(@user), params:{user:{name: "",email: "foo@invalid",password:"foo",password_combination:"bar"}}
-    assert_not_template 'user/edit'
+    patch user_path(@user), params:{user:{name: "",email: "foo@invalid",password:"foo",password_confirmation:"bar"}}
+    assert_template 'users/edit'
   end
+
+  test "successfull edit" do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    name = "Foo bar"
+    email = "foo@bar.com"
+    patch user_path(@user),params:{user:{name: name, email: email,password: "",password_confirmation:""}}
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
+  end
+  
   
 end
