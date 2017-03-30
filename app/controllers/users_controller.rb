74f36e80700d26e43260ec44bb0a29ace010ff76
JustinWeq <zoomdude111@hotmail.com>
2017-03-30 14:27:56 -0700

    Added yet more test cases

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action:logged_in_user, only:[:edit,:update]
 
   def show
     @user = User.find(params[:id])
@@ -34,6 +35,15 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+
+  #confirms that a user is logged in
+  def logged_in_user
+    unless logged_in?
+      flash[:danger]= "please log in"
+      redirect_to login_url
+    end
+  end
+  
   
 
   private
