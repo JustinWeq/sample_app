8b7fac348a738dcc150753baf79e8282936393d3
JustinWeq <zoomdude111@hotmail.com>
2017-04-03 11:46:47 -0700

    Changed some stuff

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
-User.create!(name: "Jeremy Red",email: "zoomdude111@hotmail.com",password: "zoom4000",password_confirmation:"zoom4000",admin: true)
+User.create!(name: "Jeremy Red",email: "zoomdude111@hotmail.com",password: "zoom4000",password_confirmation:"zoom4000",admin: true,activated:true,activated_at: Time.zone.now)
 
 99.times do |n|
     name = Faker::Name.name
     email = "example-#{n+1}@rails.org"
     password = "password"
-    User.create!(name:name,email: email,password:password,password_confirmation:password)
+    User.create!(name:name,email: email,password:password,password_confirmation:password,activated: true,activated_at: Time.zone.now)
 end
